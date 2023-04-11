66dc0a8f82f7e7be52823ee340e5fe1b742c65be
Ahmet YILDIZ <ayunus155@gmail.com>
2023-04-11 04:41:46 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex justify-center items-center h-screen w-screen">
+    <div class="flex items-center justify-center bg-blue-700 text-white/80 h-1/2 w-1/2">
+      <p class=" text-center text-2xl">
+        You can start from here.
+      </p>
+    </div>
+  </div>
+</template>
